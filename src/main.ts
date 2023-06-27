2c76ba338e693470e39fceacc5dd061a98b504e2
Andrea Sisti <andreasisti.26@gmail.com>
2023-06-27 09:31:21 +0200

    inclusione nel main

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import type { App } from 'vue';
-import {VrxButton, VrxInput, VrxIcon, VrxSelect} from "@/components";
+import {VrxButton, VrxInput, VrxIcon, VrxSelect, VrxGrid} from "@/components";
 export default {
     install: (app: App) => {
         app.component('VrxButton', VrxButton);
         app.component('VrxInput', VrxInput);
         app.component('VrxIcon', VrxIcon);
         app.component('VrxSelect', VrxSelect)
+        app.component('VrxGrid', VrxGrid)
     }
 };
 
-export { VrxButton, VrxInput, VrxIcon, VrxSelect };
+export { VrxButton, VrxInput, VrxIcon, VrxSelect, VrxGrid };
