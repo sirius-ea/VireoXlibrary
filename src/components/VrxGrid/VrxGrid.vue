5dc4b3a850a20a1049ebedfa575efd732037823b
Andrea Sisti <andreasisti.26@gmail.com>
2023-06-27 16:05:36 +0200

    fix test

--- src/components/VrxGrid/VrxGrid.vue
+++ src/components/VrxGrid/VrxGrid.vue
@@ -15,25 +15,28 @@
   import {Grid} from "@/components/VrxGrid/Models/Grid.ts";
 
   onMounted(() => {
-    document.getElementById('vrx-grid').addEventListener('keydown', (e) => {
-      if((e.ctrlKey || e.metaKey) && e.key === 'a'){
-        e.preventDefault();
-        gridModel.selectAll();
-      }
-    })
-    document.getElementById('vrx-grid').addEventListener('click', (e) => {
-      if(e.shiftKey && gridModel.configuration.multiselect){
-        e.preventDefault();
+    const grid = document.getElementById('vrx-grid');
+    if(grid){
+      grid.addEventListener('click', (e) => {
+        if(e.shiftKey && gridModel.configuration.multiselect){
+          e.preventDefault();
 
-        const length = gridModel.selectedRows.length;
-        if(length >= 2){
-          let index0 = gridModel.data.find((row) => row.id === gridModel.selectedRows[0].id);
-          let index1 = gridModel.data.find((row) => row.id === gridModel.selectedRows[length-1].id);
+          const length = gridModel.selectedRows.length;
+          if(length >= 2){
+            let index0 = gridModel.data.find((row) => row.id === gridModel.selectedRows[0].id);
+            let index1 = gridModel.data.find((row) => row.id === gridModel.selectedRows[length-1].id);
 
-          index0 && index1 ? gridModel.selectRange(gridModel.data.indexOf(index0), gridModel.data.indexOf(index1)) : null;
+            index0 && index1 ? gridModel.selectRange(gridModel.data.indexOf(index0), gridModel.data.indexOf(index1)) : null;
+          }
         }
-      }
-    })
+      })
+      grid.addEventListener('keydown', (e) => {
+        if((e.ctrlKey || e.metaKey) && e.key === 'a'){
+          e.preventDefault();
+          gridModel.selectAll();
+        }
+      })
+    }
   })
 
   const props = defineProps<{
