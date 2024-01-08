e4ea1591995fc208bc57e663fcc0b873ad6cc6d5
Cristian Barraco <cristian.barraco96@gmail.com>
2024-01-08 09:21:26 +0100

    Clean up e migliorato support ts

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,18 @@
 import type { App } from 'vue';
-import {VrxButton, VrxInput, VrxIcon, VrxSelect, VrxToggle, VrxGrid, VrxNavbar, VrxTree} from "@/components";
-import VrxButtonsGroup from "@/components/VrxButtonsGroup/VrxButtonsGroup.vue";
-import VrxDatePicker from "@/components/VrxDatePicker/VrxDatePicker.vue";
-import VrxDateRangePicker from "@/components/VrxDateRangePicker/VrxDateRangePicker.vue";
+import * as components from "@/components";
 
 export default {
     install: (app: App) => {
-        app.component('VrxButton', VrxButton);
-        app.component('VrxInput', VrxInput);
-        app.component('VrxIcon', VrxIcon);
-        app.component('VrxSelect', VrxSelect)
-        app.component('VrxNavbar', VrxNavbar);
-        app.component('VrxToggle', VrxToggle)
-        app.component('VrxButtonsGroup', VrxButtonsGroup)
-        app.component('VrxGrid', VrxGrid)
-        app.component('VrxDatePicker', VrxDatePicker)
-        app.component('VrxDateRangePicker', VrxDateRangePicker)
-        app.component('VrxTree', VrxTree)
-
+        app.component('VrxButton', components.VrxButton);
+        app.component('VrxInput', components.VrxInput);
+        app.component('VrxIcon', components.VrxIcon);
+        app.component('VrxSelect', components.VrxSelect)
+        app.component('VrxNavbar', components.VrxNavbar);
+        app.component('VrxToggle', components.VrxToggle)
+        app.component('VrxGrid', components.VrxGrid)
+        app.component('VrxDatePicker', components.VrxDatePicker)
+        app.component('VrxDateRangePicker', components.VrxDateRangePicker)
+        app.component('VrxTree', components.VrxTree)
     }
 };
-export {
-    VrxButton,
-    VrxInput,
-    VrxIcon,
-    VrxSelect,
-    VrxToggle,
-    VrxButtonsGroup,
-    VrxGrid,
-    VrxNavbar,
-    VrxDatePicker,
-    VrxDateRangePicker,
-    VrxTree
-};
+export * from "@/components";
