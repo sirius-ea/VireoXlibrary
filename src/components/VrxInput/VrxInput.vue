77ef41d1e241ec76b000a95b8e920a9e1c60b35c
Eddie Nomeika <nomeika@sirius-ea.com>
2025-02-24 14:16:51 +0100

    Compilation error fix

--- src/components/VrxInput/VrxInput.vue
+++ src/components/VrxInput/VrxInput.vue
@@ -63,6 +63,11 @@ const props = withDefaults(defineProps<{
 
 const showPassword = ref(false);
 
+defineSlots<{
+  default?: () => any;  // Default unnamed slot
+  right?: () => any;
+}>();
+
 const emit = defineEmits(['update:modelValue'])
 const updateValue = (event : any) => emit('update:modelValue', event.target.value)
 
